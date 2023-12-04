735433fdcacfeb2aeedc49f88db09a76ea4dc3b7
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2023-12-04 18:56:22 -0300

    Create comments.js

---
+++ aulas/aula4/comments.js
@@ -0,0 +1,14 @@
+// Comentários
+
+console.log('Olá mundo!'); // E escrever um comentário
+
+console.log('Linha 1'); // Aqui tem outro comentário
+
+/*
+console.log('Linha 2');
+console.log('Linha 3');
+*/
+
+// Vai ser ignorado
+
+console.log('Linha 4');
