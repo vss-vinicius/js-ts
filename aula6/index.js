1e0e2e1c38a3fdc8db1f2cd40374d008f670ed77
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2023-12-11 22:06:12 -0300

    Create index.js

---
+++ aula6/index.js
@@ -0,0 +1,41 @@
+// Não se pode criar variáveis com palavras reservadas
+// Não se pode começar o nome de uma variável com um número
+// Variáveis não podem conter espaços ou traços
+// Variáveis precisam ter nomes significativos
+// Utilizamos camelCase
+// Utilizamos Case-sensitive
+// Não se pode redeclarar variáveis com let
+// Tente sempre utilizar 'let' ao invés de 'var'.
+
+let nomeCliente = 'Vinicius';
+let nomecliente = 'Soares';
+console.log(nomeCliente, nomecliente);
+
+/*
+let nomeCompletoDoCliente = 'Vinicius Soares'
+console.log(nomeCompletoDoCliente);
+*/
+
+/*
+let nomeCliente = 'João';
+console.log(nomeCliente);
+*/
+
+/*
+let nome; // declaração de variável
+nome = 'João'; // inicialização de variável
+console.log(nome);
+
+nome = 'Vinicius';
+console.log(nome);
+*/
+
+/*
+let nome = 'Vinicius';
+
+console.log(`${nome} nasceu em 1984.`);
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log(`Maria teve um filho com ${nome} em 2015.`);
+console.log('O filho de', nome, 'se chama Eduardo.');
+*/
