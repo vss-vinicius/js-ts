5a0292bebc46b2ba9750761745d55fb2063577c2
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2023-12-14 18:23:55 -0300

    Create index.js

---
+++ aula7/index.js
@@ -0,0 +1,19 @@
+// Não se pode criar constantes com palavras reservadas
+// Não se pode começar o nome de uma constante com um número
+// Constantes não podem conter espaços ou traços
+// Constantes precisam ter nomes significativos
+// Utilizamos camelCase
+// Utilizamos Case-sensitive
+// Não se pode modificar o valor de uma constante
+// Utilizar 'const'
+
+
+//String = text | Number = Número
+const primeiroNumero = 5; // Number
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero; // + - * /
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(typeof (primeiroNumero + segundoNumero));
