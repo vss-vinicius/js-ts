3bd1fa014ec83c19d4f6a738cf5851dce3fa58b8
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-01-18 21:38:31 -0300

    Update indes.js

--- aula9/indes.js
+++ aula9/indes.js
@@ -1,5 +1,8 @@
 // ECMAScript 2015 (ES6) JavaScript
 
-nome = 'Vinicius'; // Não faça isso, especifique let, var ou const
+//nome = 'Vinicius'; Não faça isso, especifique let, var ou const
+
+let nome = 'Vinicius'; // Declaração
+nome = 'Soares'; // Redeclaração
 
 console.log(nome);
