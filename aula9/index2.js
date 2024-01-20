86fd112ed85d3be713617bce47caf935090fb513
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-01-20 16:47:17 -0300

    Update index2.js

---
+++ aula9/index2.js
@@ -0,0 +1,13 @@
+const nome = 'Vinicius'; //string
+const nome1 = "Vinicius"; //string
+const nome2 = `Vinicius`; //string
+
+const num1 = 10; //number
+const num2 = 10.52; //number
+
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; //nulo -> não aponta para local nenhum na memória
+
+const aprovado = false; //boolean = true or false (lógico)
+
+console.log(typeof aprovado);
