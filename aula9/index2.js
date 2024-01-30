6e5fbba1412ff102c36852fa5c6b09f6e6aa074b
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-01-29 21:48:34 -0300

    Update index2.js

--- aula9/index2.js
+++ aula9/index2.js
@@ -1,15 +1,15 @@
-// string, number, undefined, null, boolean, symbol
+//string, number, undefined, null, boolean, symbol
 
-const nome = 'Vinicius'; // string
-const nome1 = "Vinicius"; // string
-const nome2 = `Vinicius`; // string
+const nome = 'Vinicius'; //string
+const nome1 = "Vinicius"; //string
+const nome2 = `Vinicius`; //string
 
-const num1 = 10; // number
-const num2 = 10.52; // number
+const num1 = 10; //number
+const num2 = 10.52; //number
 
-let nomeAluno; // undefined -> não aponta para local nenhum na memória
-const sobrenomeAluno = null; // nulo -> não aponta para local nenhum na memória
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; //nulo -> não aponta para local nenhum na memória
 
-const aprovado = false; // boolean = true or false (lógico)
+const aprovado = false; //boolean = true or false (lógico)
 
 console.log(aprovado);
