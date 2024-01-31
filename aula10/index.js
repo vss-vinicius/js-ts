5303f3393483039f5b9af3be1f2a74cf78a98a2d
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-01-30 23:41:23 -0300

    Create index.js

---
+++ aula10/index.js
@@ -0,0 +1,24 @@
+/*
+    Operadores Aritiméticos
+
+    +    Adição | Concatenação
+    -    Subtração
+    *    Multiplicação
+    /    Divisão
+    **   Potenciação
+    %    Resto da divisão
+*/
+
+/*
+    Precedência
+
+    ()      Parênteses
+    **      Potenciação
+    * / %   Multiplicação | Divisão | Resto da divisão
+    + -     Adição | Subtração
+*/
+
+const num1 = 5
+const num2 = 2
+const num3 = 10
+console.log((num1 + num2) * num3)
