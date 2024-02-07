e7e83e1ee840b203af82e5d9e083799176bb5c8c
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-02-07 18:21:40 -0300

    Update script.js

--- aula11/js/script.js
+++ aula11/js/script.js
@@ -1,1 +1,9 @@
-alert('Com a nossa mensagem.');
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+alert (`O resultado da sua conta foi ${resultado}.`)
