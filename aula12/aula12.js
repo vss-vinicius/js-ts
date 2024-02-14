a774139bb2c2715f6ad45ed4538dbc0f8305acfd
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-02-14 17:39:37 -0300

    Update aula12.js

--- aula12/aula12.js
+++ aula12/aula12.js
@@ -2,9 +2,13 @@ let varA = 'A'; // B
 let varB = 'B'; // C
 let varC = 'C'; // A
 
+/*
 const varATemp = varA;
 varA = varB;
 varB = varC;
 varC = varATemp;
+*/
+
+[varA, varB, varC] = [varB, varC, varA]
 
 console.log(varA, varB, varC);
