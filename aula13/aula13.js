5c09ea2a3678636ed2fc6e8dd2e6c0c629e2d944
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-02-19 21:45:45 -0300

    Update aula13.js

--- aula13/aula13.js
+++ aula13/aula13.js
@@ -1,3 +1,39 @@
-let umaString = "Um \"texto\"";
+// let umaString = "Um \"texto\"";
 
-console.log(umaString)
+// let umaString = "Um \\texto";
+
+/*
+let umaString = "Um texto";
+
+console.log(umaString.concat(' em um lindo dia.'))
+console.log(umaString + ' em um lindo dia.')
+console.log(`${umaString} em um lindo dia.`)
+*/
+
+let umaString = "O rato roeu a roupa do rei de roma.";
+
+// console.log(umaString.indexOf('Um'))
+
+// console.log(umaString.lastIndexOf('0', 3));
+
+// console.log(umaString.match(/[a-z]/g));
+
+// console.log(umaString.search(/x/));
+
+// console.log(umaString.replace(/r/g, '#'));
+
+// console.log(umaString.length);
+
+// console.log(umaString.slice(2, 6));
+
+// console.log(umaString.slice(-3));
+
+// console.log(umaString.slice(-5, - 1));
+
+// console.log(umaString.substring(umaString.length - 5, umaString.length - 1));
+
+// console.log(umaString.split(' ', 3));
+
+console.log(umaString.toUpperCase());
+
+console.log(umaString.toLowerCase());
