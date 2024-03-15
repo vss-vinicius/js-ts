142b280ce7160bcb44cc7be62295ed9abab55fd0
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-03-15 14:40:58 -0300

    Update index.js

---
+++ aula14/index.js
@@ -0,0 +1,14 @@
+// IEEE 754-2008
+
+let num1 = 0.7; // number
+let num2 = 0.1; // number
+
+console.log(num1 + num2);
+
+// console.log(num1.toString() + num2);
+// num1 = num1.toString();
+//console.log(num1.toString(2));
+//console.log(num1.toFixed(2));
+//console.log(Number.isInteger(num1));
+//console.log(Number.isNaN(temp));
+//let temp = num1 + '5';
