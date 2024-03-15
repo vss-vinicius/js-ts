71f1aeff3a4a600ae9f06d688761830536410e6a
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-03-15 14:54:36 -0300

    Update index.js

--- aula14/index.js
+++ aula14/index.js
@@ -3,7 +3,21 @@
 let num1 = 0.7; // number
 let num2 = 0.1; // number
 
-console.log(num1 + num2);
+num1 += num2; // num1 = num1 + num2;
+num1 += num2; // 0.9
+num1 += num2; // 1.0
+num1 += num2; // 1.1
+
+/*
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.8
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 0.9
+num1 = ((num1 * 100) + (num2 * 100)) / 100; // 1.0
+*/
+
+num1 = Number(num1.toFixed(2));
+
+console.log(num1);
+console.log(Number.isInteger(num1));
 
 // console.log(num1.toString() + num2);
 // num1 = num1.toString();
