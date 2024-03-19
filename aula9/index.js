b37ec7d5f81852625ae97c287d6af571cf1ca3f0
Vinicius Soares <100010777+vss-vinicius@users.noreply.github.com>
2024-03-18 23:00:14 -0300

    Update index.js

--- aula9/index.js
+++ aula9/index.js
@@ -1,6 +1,6 @@
 // ECMAScript 2015 (ES6) JavaScript
 
-//nome = 'Vinicius'; Não faça isso, especifique let, var ou const
+// nome = 'Vinicius'; Não faça isso, especifique let, var ou const
 
 let nome = 'Vinicius'; // Declaração
 nome = 'Soares'; // Redeclaração
